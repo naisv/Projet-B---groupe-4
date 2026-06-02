fb2ce6f6660ed1adb3278f35a459d4f2119aa7f7
axelw <axel.walraet-triolet.1@ens.etsmtl.ca>
2026-06-02 15:55:48 -0400

    creation de la fonction integration_rectangle qui retourne l'aire de la fonction selon les bornes et le pas choisi

--- methode_rectangle.py
+++ methode_rectangle.py
@@ -1,1 +1,18 @@
 #Module de la méthode des rectangles
+
+from solution_analytique import polynome
+
+def integration_rectangle(a_inf, b_sup, n):
+    """
+    Calcule l'intégrale numérique par la méthode des rectangles.
+    """
+    h = (b_sup - a_inf) / n
+    somme_aires = 0.0
+
+    for i in range(n):
+        x_i = a_inf + i * h
+        # Évaluation de la fonction de degré 3 au point x_i
+        f_xi = polynome(x_i)
+        somme_aires += f_xi * h
+
+    return somme_aires
