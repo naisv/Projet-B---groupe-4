50c1748b6473fef29e68561d8639fb59cb7433b5
axelw <axel.walraet-triolet.1@ens.etsmtl.ca>
2026-06-02 18:18:06 -0400

    Ajout d'une fonction de calcul d'erreur

--- performance.py
+++ performance.py
@@ -1,1 +1,10 @@
 #Calcul de la performance des differentes methodes
+
+from solution_analytique import compute_solution_analytique
+from methode_rectangle import *
+
+def compute_erreur(a, b, n, fonction):
+    """
+    fonction qui calcule l'erreur entre la methode choisie et la valeur analytique
+    """
+    return compute_solution_analytique(a,b) - fonction(a,b,n)
