a6dda862cc1a3a6b9d4873731c657fde0420e5c2
Paul Serra <paul.serra.1@ens.etsmtl.ca>
2026-06-02 15:56:12 -0400

    Ajout fonction base simpson

--- methode_simpson.py
+++ methode_simpson.py
@@ -1,1 +1,15 @@
 #Module de la méthode simpson
+
+from solution_analytique import polynome
+
+def simpson_basique(a, b, n=10):
+    pas=(b-a)/n
+    u=a #correspond au "a" de la formule dans l'énoncé du devoir
+    v=a+pas #correspond au "b" de la formule dans l'énoncé du devoir
+    aire=0
+    for i in range(n):
+        aire+=(v-u)/6*(polynome(u)+4*polynome((u+v)/2)+polynome(v))
+        #On avance d'un pas
+        u=v
+        v+=pas
+    return aire
