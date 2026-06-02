dc9462fd33030779a421b771510fcc3886699fa0
Vigroux Nais <nais.vigroux@gmail.com>
2026-06-02 16:26:11 -0400

    création de la fonction trapèze avec numpy

--- methode_trapeze.py
+++ methode_trapeze.py
@@ -1,5 +1,8 @@
 #Module de la méthode des trapèzes
 
+#Importation de la bibliothèque Numpy
+import numpy as np
+
 #Importation des fonctions qui retournent le polynome et son intégrale exacte (solution analytique)
 from solution_analytique import polynome, compute_solution_analytique
 
@@ -14,3 +17,13 @@ def trap_python(a,b,n):
         trapeze=(borne_sup-borne_inf)*(polynome(borne_sup)+polynome(borne_inf))/2
         somme+=trapeze
     return somme
+
+#Fonction calculant l'intégrale entre [a,b] fixé en paramètre selon la méthode des trapèzes en utilisant Numpy
+def trap_numpy(a,b,n):
+    x=np.linspace(a,b,n+1) #tableau avec n valeurs uniformément réparties entre a et b
+    y=polynome(x) #tableau avec les images de x de la fonction polynome
+    h=(b-a)/n
+    integrale=h*(np.sum(y)-0.5*(y[0]+y[-1]))
+    return integrale
+
+
